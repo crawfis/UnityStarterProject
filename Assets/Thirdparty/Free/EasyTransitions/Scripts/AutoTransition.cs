7986edfde9f7771afea75404514d39b2f5d67111
Roger Crawfis <roger.crawfis@gmail.com>
2024-09-12 12:02:58 -0400

    Added the Easy Transitions package.

---
+++ Assets/Thirdparty/Free/EasyTransitions/Scripts/AutoTransition.cs
@@ -0,0 +1,18 @@
+﻿using EasyTransition;
+
+using UnityEngine;
+
+namespace CrawfisSoftware
+{
+    internal class AutoTransition : MonoBehaviour
+    {
+        public TransitionSettings transition;
+        public float _transitionDelay = 2f;
+        public string _scene = "TransitionDemo";
+
+        private void Start()
+        {
+            TransitionManager.Instance().Transition(_scene, transition, _transitionDelay);
+        }
+    }
+}
